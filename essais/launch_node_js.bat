b7e6073f55cd3d0437254bc5565ea7a3ef508251
didier <didier@d-defrance.fr>
2016-01-11 15:22:30 +0100

    test prototype

--- essais/launch_node_js.bat
+++ essais/launch_node_js.bat
@@ -2,5 +2,6 @@ REM node -v
 REM node hello_world.js
 REM node first_http_server.js
 REM http://localhost:8282/addition?a=3&b=2
-node addition_http_server.js
+REM node addition_http_server.js
+node test_javascript_prototype.js
 pause

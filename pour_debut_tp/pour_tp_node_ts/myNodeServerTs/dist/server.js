07813d99e9dcfdc57799075979679fa14e8268e5
didier defrance <didier-defrance@wanadoo.fr>
2020-06-03 18:19:42 +0200

    pour debut tp

--- pour_debut_tp/pour_tp_node_ts/myNodeServerTs/dist/server.js
+++ pour_debut_tp/pour_tp_node_ts/myNodeServerTs/dist/server.js
@@ -3,14 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
 const bodyParser = tslib_1.__importStar(require("body-parser"));
-const fileUpload = require('express-fileupload');
 exports.app = express_1.default();
-const apiHandler_1 = require("./api/apiHandler");
+//import { apiErrorHandler} from './api/apiHandler'
 const globalRoutes_1 = require("./api/globalRoutes");
 const deviseApiRoutes_1 = require("./api/deviseApiRoutes");
-const publicationApiRoutes_1 = require("./api/publicationApiRoutes");
-const loginApiRoutes_1 = require("./api/loginApiRoutes");
-const verif_auth_1 = require("./api/verif-auth");
+//import { loginApiRouter } from './api/loginApiRoutes';
+//import { verifTokenInHeadersForPrivatePath, secureModeApiRouter } from './api/verif-auth';
 const MyAppConfig_1 = require("./config/MyAppConfig");
 //PRE TRAITEMENTS (à placer en haut de server.ts)
 //support parsing of JSON post data
@@ -18,10 +16,6 @@ var jsonParser = bodyParser.json();
 exports.app.use(jsonParser);
 exports.app.use(bodyParser.urlencoded({
     extended: true
-}));
-//support for fileUpload:
-exports.app.use(fileUpload({
-    limits: { fileSize: 5 * 1024 * 1024 },
 }));
 //renvoyer directement les pages statiques rangées dans le répertoire "front-end":
 exports.app.use(express_1.default.static('front-end'));
@@ -31,15 +25,13 @@ exports.app.use(function (req, res, next) {
     next();
 });
 //verif auth beared token in request for private api/path:
-exports.app.use(verif_auth_1.verifTokenInHeadersForPrivatePath);
+//app.use(verifTokenInHeadersForPrivatePath);
 //ROUTES ORDINAIRES (apres PRE traitements , avant POST traitements)
 exports.app.use(globalRoutes_1.globalRouter); //delegate some  global express routes
-exports.app.use(verif_auth_1.secureModeApiRouter); //dev-only ( http://localhost:8282/auth-api/dev-only/secure/true or false)
 exports.app.use(deviseApiRoutes_1.deviseApiRouter); //delegate some REST API routes
-exports.app.use(publicationApiRoutes_1.publicationApiRouter); //delegate some REST API routes
-exports.app.use(loginApiRoutes_1.loginApiRouter); //delegate some REST API routes  
+//app.use(loginApiRouter);  //delegate some REST API routes  
 //POST TRAITEMENTS (à placer en bas de server.ts):
-exports.app.use(apiHandler_1.apiErrorHandler); //pour gérer les erreurs/exceptions
+//app.use(apiErrorHandler); //pour gérer les erreurs/exceptions
 //automatiquement rattrapées .then().catch() de asyncToResp (api/apiHandler.ts)
 /*exemple : deviseApiRouter.route('/devise/:code')
                .get(asyncToResp(async function(req :Request, res :Response , next: NextFunction){
@@ -50,16 +42,8 @@ exports.app.use(apiHandler_1.apiErrorHandler); //pour gérer les erreurs/excepti
 */
 exports.server = exports.app.listen(process.env.PORT, function () {
     console.log("http://localhost:" + process.env.PORT);
-    //console.log("process.argv="+ process.argv);
     if (MyAppConfig_1.MyAppConfig.isNoDB()) {
         console.log("starting with memoryMap services (no database)");
-    }
-    else {
-        /*
-        //if not called here , initConnections() will be deffered (lazy)
-        myAppConnectionMap.initConnections()
-                          .then((bOk)=>{ console.log("database connections is ok"); });
-        */
     }
     console.log("rest express node server listening at " + process.env.PORT);
 });
--- pour_debut_tp/pour_tp_node_ts/myNodeServerTs/src/server.ts
+++ pour_debut_tp/pour_tp_node_ts/myNodeServerTs/src/server.ts
@@ -1,13 +1,11 @@
 import express  from 'express';
 import * as bodyParser from 'body-parser';
-const fileUpload  = require('express-fileupload');
 export const  app :express.Application = express();
-import { apiErrorHandler} from './api/apiHandler'
+//import { apiErrorHandler} from './api/apiHandler'
 import { globalRouter } from './api/globalRoutes';
 import { deviseApiRouter } from './api/deviseApiRoutes';
-import { publicationApiRouter } from './api/publicationApiRoutes';
-import { loginApiRouter } from './api/loginApiRoutes';
-import { verifTokenInHeadersForPrivatePath, secureModeApiRouter } from './api/verif-auth';
+//import { loginApiRouter } from './api/loginApiRoutes';
+//import { verifTokenInHeadersForPrivatePath, secureModeApiRouter } from './api/verif-auth';
 import { MyAppConfig } from './config/MyAppConfig';
 
 
@@ -21,10 +19,7 @@ app.use(bodyParser.urlencoded({
     extended: true
   }));
 
-//support for fileUpload:
-app.use(fileUpload({
-    limits: { fileSize: 5 * 1024 * 1024 },
-  }));
+
 
 //renvoyer directement les pages statiques rangées dans le répertoire "front-end":
 app.use(express.static('front-end'));
@@ -36,19 +31,17 @@ app.use(function(req, res, next) {
 });
 
 //verif auth beared token in request for private api/path:
-app.use(verifTokenInHeadersForPrivatePath);
+//app.use(verifTokenInHeadersForPrivatePath);
 
 //ROUTES ORDINAIRES (apres PRE traitements , avant POST traitements)
 
 app.use(globalRouter); //delegate some  global express routes
-app.use(secureModeApiRouter); //dev-only ( http://localhost:8282/auth-api/dev-only/secure/true or false)
 app.use(deviseApiRouter);  //delegate some REST API routes
-app.use(publicationApiRouter);  //delegate some REST API routes
-app.use(loginApiRouter);  //delegate some REST API routes  
+//app.use(loginApiRouter);  //delegate some REST API routes  
 
 //POST TRAITEMENTS (à placer en bas de server.ts):
 
-app.use(apiErrorHandler); //pour gérer les erreurs/exceptions
+//app.use(apiErrorHandler); //pour gérer les erreurs/exceptions
                           //automatiquement rattrapées .then().catch() de asyncToResp (api/apiHandler.ts)
          /*exemple : deviseApiRouter.route('/devise/:code')
                         .get(asyncToResp(async function(req :Request, res :Response , next: NextFunction){
@@ -59,16 +52,9 @@ app.use(apiErrorHandler); //pour gérer les erreurs/exceptions
 */
 
 export const server = app.listen(process.env.PORT , function () {
-    console.log("http://localhost:" + process.env.PORT );
-    //console.log("process.argv="+ process.argv);
+    console.log("http://localhost:" + process.env.PORT );        
     if(MyAppConfig.isNoDB()){
       console.log("starting with memoryMap services (no database)"); 
-    }else{
-      /*
-      //if not called here , initConnections() will be deffered (lazy)
-      myAppConnectionMap.initConnections()
-                        .then((bOk)=>{ console.log("database connections is ok"); });               
-      */   
-    }                    
+    }	
     console.log("rest express node server listening at " + process.env.PORT);
 });
